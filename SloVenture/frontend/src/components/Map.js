069a232d4db0fe8bb376dd6c18f5dbd5d427b93b
nikavuk <nika.vuk@student.um.si>
2025-05-13 20:43:39 +0200

    SCRUM-102 Priprava zasnove strani za zemljevid

--- SloVenture/frontend/src/components/Map.js
+++ SloVenture/frontend/src/components/Map.js
@@ -1,6 +1,13 @@
 function Map() {
   return (
-      <p>Tu bo prikazan zemljevid</p>
+    <div id="map">
+      <img
+        src="http://localhost:3001/images/placeholder_slovenija.jpg"
+        alt="Zemljevid Slovenije"
+        style={{ width: '100%', maxWidth: '600px', borderRadius: '12px' }}
+      />
+      <p style={{ marginTop: '8px' }}>To je začasni prikaz – končni zemljevid bo interaktiven z D3.js.</p>
+    </div>
   );
 }
 
